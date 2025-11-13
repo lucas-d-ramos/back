43e566bdeeccaec1f63336714716cd6461b9c250
Lucas Ramos <lucas_r@me.com>
2025-11-13 11:42:14 +0100

    ..

---
+++ actionkit/scenarios/thanks.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    label: "thanks",
+    url: "https://action.wemove.eu/thanks/developers-testing-campaign-tweet-EN?done=1",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [".grecaptcha-badge"],
+  },
+  {
+    label: "thanks--germanic",
+    url: "https://action.wemove.eu/thanks/basic-wemove-email-consent-en?action_id=4543575&akid=a4543571.945919.tXEf3t&ar=1&done=1&rd=1",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [".grecaptcha-badge"],
+  },
+];
---
+++ actionkit/scenarios/youmove-petition.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    label: "youmove_petition---UNKNOWN_USER",
+    url: "https://action.wemove.eu/sign/developers-testing-campaign-petition-ES/",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [
+      ".grecaptcha-badge",
+      "[data-wm-floating-meter]",
+      "[data-wm-recent-action]",
+      "[data-wm-recent-comments]",
+    ],
+  },
+  {
+    label: "youmove_petition---KNOWN_USER_NEVER",
+    url: "https://action.wemove.eu/sign/developers-testing-campaign-petition-ES?akid=.1172942.fL7c2K",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [
+      ".grecaptcha-badge",
+      "[data-wm-floating-meter]",
+      "[data-wm-recent-action]",
+      "[data-wm-recent-comments]",
+    ],
+  },
+  {
+    label: "youmove_petition---KNOWN_USER_SUBSCRIBED",
+    url: "https://action.wemove.eu/sign/developers-testing-campaign-petition-ES/?akid=.1172956.YowR8V",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [
+      ".grecaptcha-badge",
+      "[data-wm-floating-meter]",
+      "[data-wm-recent-action]",
+      "[data-wm-recent-comments]",
+    ],
+  },
+  {
+    label: "youmove_petition---KNOWN_USER_WHO_TOOK_ACTION",
+    url: "https://action.wemove.eu/sign/developers-testing-campaign-petition-ES?action_id=2075869&akid=.15940.jgbvgp&share=1",
+    cookiePath: "actionkit/reports/engine_scripts/cookie-bot.json",
+    hideSelectors: [
+      ".grecaptcha-badge",
+      "[data-wm-floating-meter]",
+      "[data-wm-recent-action]",
+      "[data-wm-recent-comments]",
+    ],
+  },
+];
